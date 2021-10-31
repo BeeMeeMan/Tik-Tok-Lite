0700c428d6f63bfcd469a659e357628907da6ab4
Jenya <Korsun>
2021-10-31 13:33:54 +0100

    Initial Commit

---
+++ Tik-Tok Lite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tik-Tok Lite
+//
+//  Created by user206820 on 10/31/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tik-Tok Lite/Tik_Tok_LiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tik_Tok_LiteApp.swift
+//  Tik-Tok Lite
+//
+//  Created by user206820 on 10/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Tik_Tok_LiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
